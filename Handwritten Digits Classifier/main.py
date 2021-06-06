b88e28bb2e8770ee710c6fe95b2b22aa84c60f33
Yash Joshi <57494405+ypjoshi18@users.noreply.github.com>
2021-06-06 17:00:18 +0530

    Create main.py

---
+++ Handwritten Digits Classifier/main.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+loss, accuracy = model.evaluate(x_test, y_test)
+print(accuracy, loss)
+
+model.save('digits.model')
+
+# For output run for loop after training theh model
+
+# for x in range(1,20):
+#   img = cv.imread(f'{x}.png')[:,:,0]
+#   img = np.invert(np.array([img]))
+#   prediction = model.predict(img)
+#   print(np.argmax(prediction))
+#   plt.imshow(img[0], cmap=plt.cm.binary)
+#   plt.show()
