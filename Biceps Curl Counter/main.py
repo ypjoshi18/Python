9063ee4fd58760db56e00bd0cef807318059b42b
Yash Joshi <57494405+ypjoshi18@users.noreply.github.com>
2021-12-11 20:57:47 +0530

    main file

---
+++ Biceps Curl Counter/main.py
@@ -0,0 +1,12 @@
+import app
+
+
+def print_hi():
+    app.App()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
